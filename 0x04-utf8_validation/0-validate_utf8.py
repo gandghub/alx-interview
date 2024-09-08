0c35e64c48c491972c8f226ac815f64d95d7542d
gandghub <hackthehacker33@gmail.com>
2024-09-08 13:46:34 +0000

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,57 +1,38 @@
 #!/usr/bin/python3
-"""Script for UTF-8 validator function."""
+"""
+Method to determine if given data represents valid UTF-8 encoding
+Prototype: def validUTF8(data)
+Returns True if data is valid UTF-8 encoding, else return False
+Dataset can contain multiple characters
+Data will represent a list of integers
+"""
 
 
 def validUTF8(data):
     """
-        Determines if a given data set represents a valid UTF-8 encoding.
-        Returns True if data is a valid UTF-8 encoding, otherwise False.
+    Prototype: def validUTF8(data)
+    Returns True if data is valid UTF-8 encoding
+    else return False
     """
-    # 8-bit and 4-max-1s consts
-    # Loop over data list and:
-    # Extract each bit in an AND BITWISE OP = index
-    # Shift index by (8-bit - 1) in a SIGNED RT SHFT(>>) OP
-    # Validate utf-8 encoding pattern
-    # In an inner loop(RT SHFT thro' index(bit) until 0 is encountered),
-    # If char is single byte, continue to other chars in data list.
-    # Sum up the consec-1s and compare it with 4-max-1s
-    # Return False if consec-1s > 4-max-1s or eq to 1(Invalid utf-8)
-    # Validate consec-1s
-    # For multi-bytes char, check for next bytes if they start with 1
-    # followed by 0
-    # Return True if all the validation checks are met.
+    count = 0
 
-    NUMBER_OF_BITS_PER_BLOCK = 8
-    MAX_NUMBER_OF_ONES = 4
-    index = 0
-    while index < len(data):
-        number = data[index] & (2 ** 7)
-        number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-        if number == 0:
-            index += 1
-            continue
-        number_of_ones = 0
-        while True:
-            number = data[index] & (2 ** (7 - number_of_ones))
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - number_of_ones - 1)
-            if number == 1:
-                number_of_ones += 1
-            else:
-                break
-            if number_of_ones > MAX_NUMBER_OF_ONES:
+    for bit in data:
+        binary = bin(bit).replace('0b', '').rjust(8, '0')[-8:]
+        if count == 0:
+            if binary.startswith('110'):
+                count = 1
+            if binary.startswith('1110'):
+                count = 2
+            if binary.startswith('11110'):
+                count = 3
+            if binary.startswith('10'):
                 return False
-        if number_of_ones == 1:
-            return False
-        index += 1
-        if index >= len(data) or index >= (index + number_of_ones - 1):
-            return False
-        for i in range(index, index + number_of_ones - 1):
-            number = data[i]
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number != 1:
+        else:
+            if not binary.startswith('10'):
                 return False
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-            if number != 0:
-                return False
-            index += 1
+            count -= 1
+
+    if count != 0:
+        return False
+
     return True
